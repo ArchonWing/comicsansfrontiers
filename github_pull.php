5f3fbfe44352eb385bd2ae64d6553502af916be6
Nathan Berger <nberger21@gmail.com>
2016-11-22 23:34:41 -0500

    test

--- github_pull.php
+++ github_pull.php
@@ -15,5 +15,5 @@ file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
 if ($payload->ref === 'refs/heads/master')
 {
   // path to your site deployment script
-  exec('reboot');
+  exec('git pull');
 }
