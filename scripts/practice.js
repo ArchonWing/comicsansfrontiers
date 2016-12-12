a30e7b87b0a81b325a7396db2efb65c97a655381
nberger30 <nberger21@gmail.com>
2016-12-12 17:47:26 -0500

    Edited JS script again

--- scripts/practice.js
+++ scripts/practice.js
@@ -4,7 +4,7 @@ myHeading.textContent = 'Move Faster!';
 myHeading.onclick = function(){
   var message = myHeading.textContent;
   if (message === 'Move Faster!'){
-    myHeading.textContent = 'I\'m already moving fast!';
+    myHeading.textContent = 'I\'m already moving fast!!!';
   } else {
     myHeading.textContent = 'Move Faster!';
   }
