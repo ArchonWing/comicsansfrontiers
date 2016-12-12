b0a04aaa8898dc45446011bf8f5b1d0950a12546
nberger30 <nberger21@gmail.com>
2016-12-12 17:46:45 -0500

    Edited js script

--- scripts/practice.js
+++ scripts/practice.js
@@ -2,12 +2,10 @@ var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Move Faster!';
 
 myHeading.onclick = function(){
-
   var message = myHeading.textContent;
   if (message === 'Move Faster!'){
     myHeading.textContent = 'I\'m already moving fast!';
   } else {
     myHeading.textContent = 'Move Faster!';
   }
-
 }
